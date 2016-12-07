169417940ab2af205b48db11b8d566f95a2c6ff3
daniel-anderson-dev <daniel.anderson28562@hotmail.com>
2016-12-07 15:36:23 -0500

    Basic scoping example.

--- TheWorld/src/TheWorld/wwwroot/js/site.js
+++ TheWorld/src/TheWorld/wwwroot/js/site.js
@@ -1,17 +1,25 @@
 ﻿
 // site.js
 
+function startup()
+{
+
+
 var ele = document.getElementById('username');
 ele.innerHTML = 'Text Here';
 
 var main = document.getElementById('main');
 
 main.onmouseenter = function () {
-	//main.style.backgroundColor = '#888';
-	main.style = 'background-color: #888';
+	main.style.backgroundColor = '#888';
+	//main.style = 'background-color: #888';
 };
 
 main.onmouseleave = function () {
-	//main.style.backgroundColor = '';
-	main.style = '';
+	main.style.backgroundColor = '';
+	//main.style = '';
 };
+
+}
+
+startup();
