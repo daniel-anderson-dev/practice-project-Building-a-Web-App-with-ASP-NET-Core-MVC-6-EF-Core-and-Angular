5fdeab0ab0ac12990484e6b833208a16a565fcc6
daniel-anderson-dev <daniel.anderson28562@hotmail.com>
2016-12-07 16:25:40 -0500

    Pulled in some javascript libraries and setup a basic jquery example.

--- TheWorld/src/TheWorld/wwwroot/js/site.js
+++ TheWorld/src/TheWorld/wwwroot/js/site.js
@@ -3,19 +3,28 @@
 
 (function () {
 
-	var ele = document.getElementById('username');
-	ele.innerHTML = 'Text Here';
+	var ele = $('#username');
+	ele.text('Text Here');
 
-	var main = document.getElementById('main');
+	var main = $('#main');
+	main.on('mouseenter', function () {
+		//main.style.backgroundColor = '#888';
 
-	main.onmouseenter = function () {
-		main.style.backgroundColor = '#888';
-		//main.style = 'background-color: #888';
-	};
+		// Does not work with jQuery ...
+		main.style = 'background-color: 888;';
+	});
 
-	main.onmouseleave = function () {
-		main.style.backgroundColor = '';
-		//main.style = '';
-	};
+	main.on('mouseleave', function () {
+		//main.style.backgroundColor = '';
+		main.style = '';
+	});
+
+	var menuItems = $('ul.menu li a');
+	menuItems.on('click', function () {
+		var me = $(this);
+		
+		alert(me.text());
+	});
+
 
 })();
