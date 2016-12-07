a00a7fc4e6bd70de75911821ed1c16931990edcd
daniel-anderson-dev <daniel.anderson28562@hotmail.com>
2016-12-07 15:34:14 -0500

    Basic javascript.

---
+++ TheWorld/src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿
+// site.js
+
+var ele = document.getElementById('username');
+ele.innerHTML = 'Text Here';
+
+var main = document.getElementById('main');
+
+main.onmouseenter = function () {
+	//main.style.backgroundColor = '#888';
+	main.style = 'background-color: #888';
+};
+
+main.onmouseleave = function () {
+	//main.style.backgroundColor = '';
+	main.style = '';
+};
