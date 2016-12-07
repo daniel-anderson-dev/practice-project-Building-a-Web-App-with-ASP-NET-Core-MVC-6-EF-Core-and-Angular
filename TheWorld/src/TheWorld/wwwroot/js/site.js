42ccce386340de5f9ad9fb329e2ddf76dfcb874f
daniel-anderson-dev <daniel.anderson28562@hotmail.com>
2016-12-07 16:55:24 -0500

    Added a toggling and transition of the left navigation bar.

--- TheWorld/src/TheWorld/wwwroot/js/site.js
+++ TheWorld/src/TheWorld/wwwroot/js/site.js
@@ -3,28 +3,37 @@
 
 (function () {
 
-	var ele = $('#username');
-	ele.text('Text Here');
+	//var ele = $('#username');
+	//ele.text('Text Here');
 
-	var main = $('#main');
-	main.on('mouseenter', function () {
-		//main.style.backgroundColor = '#888';
+	//var main = $('#main');
+	//main.on('mouseenter', function () {
+	//	//main.style.backgroundColor = '#888';
 
-		// Does not work with jQuery ...
-		main.style = 'background-color: 888;';
-	});
+	//	// Not working.... moving on.
+	//	main.style = 'background-color: 888;';
+	//});
 
-	main.on('mouseleave', function () {
-		//main.style.backgroundColor = '';
-		main.style = '';
-	});
+	//main.on('mouseleave', function () {
+	//	//main.style.backgroundColor = '';
+	//	main.style = '';
+	//});
 
-	var menuItems = $('ul.menu li a');
-	menuItems.on('click', function () {
-		var me = $(this);
+	//var menuItems = $('ul.menu li a');
+	//menuItems.on('click', function () {
+	//	var me = $(this);
 		
-		alert(me.text());
+	//	alert(me.text());
+	//});
+
+	var $sidebarAndWrapper = $('#sidebar,#wrapper');
+	$('#sidebarToggle').on('click', function () {
+		$sidebarAndWrapper.toggleClass('hide-sidebar');
+		if ($sidebarAndWrapper.hasClass('hide-sidebar')) {
+			$(this).text('Show Sidebar');
+		}
+		else
+		{ $(this).text('Hide Sidebar'); }
 	});
-
-
+	
 })();
