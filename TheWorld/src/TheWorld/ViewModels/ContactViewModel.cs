0d1d4887b57311e5127772a6a70c626f63b26d01
daniel-anderson-dev <daniel.anderson28562@hotmail.com>
2016-12-09 13:47:14 -0500

    * Created a ViewModel for the Contact Page. * Added a form to the Contact Page.

---
+++ TheWorld/src/TheWorld/ViewModels/ContactViewModel.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace TheWorld.ViewModels
+{
+    public class ContactViewModel
+    {
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public string Message { get; set; }
+    }
+}
