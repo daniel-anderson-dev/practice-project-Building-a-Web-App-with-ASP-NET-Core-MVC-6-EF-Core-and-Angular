faafb42fdbcf1f07d578e387058766c76111df27
daniel-anderson-dev <daniel.anderson28562@hotmail.com>
2016-12-09 16:10:45 -0500

    Contact Page changes: * Added a POST method. * Added various forms of server-side validation. * Added an email simulation capability. * Created a "Mail Service" interface and a "debug" implementation. * Added various forms of dependency injection, configuration, etc.

--- TheWorld/src/TheWorld/Startup.cs
+++ TheWorld/src/TheWorld/Startup.cs
@@ -1,16 +1,47 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using TheWorld.Services;
 
 namespace TheWorld
 {
     public class Startup
     {
+		private IHostingEnvironment _env;
+		private IConfigurationRoot _config;
+
+		public Startup(IHostingEnvironment env)
+		{
+			// Apparently IHostingEnvironment is not accessible within ConfigureServices but apparently within Configure.
+			_env = env;
+
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(_env.ContentRootPath)
+				.AddJsonFile("config.json")
+				.AddEnvironmentVariables();
+
+			_config = builder.Build();
+		}
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+			// "Sinleton" given no need to create multiple instances.
+			services.AddSingleton(_config);
+
+			// "Production" may not be setup at the moment.
+			if (_env.IsProduction() == false)
+			{
+				services.AddScoped<IMailService, DebugMailService>();
+			}
+			else
+			{
+				// Implement real email sending functionality.
+			}
+
 			services.AddMvc();
         }
 
