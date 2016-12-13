7049a9e17667e7a26525fe0d5bd7452d62335b86
daniel-anderson-dev <daniel.anderson28562@hotmail.com>
2016-12-13 16:02:14 -0500

    Various adjustments to the layout using Bootstrap.

--- TheWorld/src/TheWorld/wwwroot/js/site.js
+++ TheWorld/src/TheWorld/wwwroot/js/site.js
@@ -29,11 +29,11 @@
 	var $sidebarAndWrapper = $('#sidebar,#wrapper');
 	$('#sidebarToggle').on('click', function () {
 		$sidebarAndWrapper.toggleClass('hide-sidebar');
-		if ($sidebarAndWrapper.hasClass('hide-sidebar')) {
-			$(this).text('Show Sidebar');
-		}
-		else
-		{ $(this).text('Hide Sidebar'); }
+		//if ($sidebarAndWrapper.hasClass('hide-sidebar')) {
+		//	$(this).text('Show Sidebar');
+		//}
+		//else
+		//{ $(this).text('Hide Sidebar'); }
 	});
 	
 })();
