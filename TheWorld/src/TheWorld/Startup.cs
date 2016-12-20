86211c1da2205e8a6ae460b69979725baf10e20e
daniel-anderson-dev <daniel.anderson28562@hotmail.com>
2016-12-20 14:56:48 -0500

    * Setup the basics for the repository pattern and added dependency injection / configuration accordingly. * Added razor code to display "trips" within the Index view.

--- TheWorld/src/TheWorld/Startup.cs
+++ TheWorld/src/TheWorld/Startup.cs
@@ -44,6 +44,7 @@ namespace TheWorld
 			}
 
             services.AddDbContext<WorldContext>();
+            services.AddScoped<IWorldRepository, WorldRepository>();
             services.AddTransient<WorldContextSeedData>();
 			services.AddMvc();
         }
