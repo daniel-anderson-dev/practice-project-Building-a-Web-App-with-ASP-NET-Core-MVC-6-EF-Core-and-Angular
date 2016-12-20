3cbcf41cbc07c08e31566bc3d03a25559e873add
daniel-anderson-dev <daniel.anderson28562@hotmail.com>
2016-12-20 15:31:22 -0500

    Added a basic Web API controller that returns hard-coded data.

---
+++ TheWorld/src/TheWorld/Controllers/Api/TripsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheWorld.Models;
+
+namespace TheWorld.Controllers.Api
+{
+    public class TripsController: Controller
+    {
+        [HttpGet("api/trips")]
+        public IActionResult Get()
+        {
+            // Demonstrates the idea of returning an "Action Result"
+            //  in case you don't always return raw json data.
+            if (true) return BadRequest("NOOO");
+            return Ok(new Trip() { Name = "My Trip" });
+        }
+    }
+}
