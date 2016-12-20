429418c12f6170314e6fe06e12abcdc9d9a9e87b
daniel-anderson-dev <daniel.anderson28562@hotmail.com>
2016-12-20 16:41:59 -0500

    Created a basic TripViewModel for a one-off validation scope that is custom to the API > Trips POST end-point.

--- TheWorld/src/TheWorld/Controllers/Api/TripsController.cs
+++ TheWorld/src/TheWorld/Controllers/Api/TripsController.cs
@@ -25,9 +25,14 @@ namespace TheWorld.Controllers.Api
         }
 
         [HttpPost("")]
-        public IActionResult Post([FromBody] Trip trip)
+        public IActionResult Post([FromBody] TripViewModel trip)
         {
-            return Ok(trip.Name);
+            if (ModelState.IsValid)
+            {
+                return Created($"api/trips/{trip.Name}", trip);
+            }
+
+            return BadRequest("Bad data");
         }
     }
 }
